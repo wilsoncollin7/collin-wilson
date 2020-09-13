9a1c5e38180796e450838dfcccbea12708b99c55
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-13 19:20:47 -0400

    updated projects section some more addes more js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,14 @@
-$( document ).ready(function() {
-    new WOW().init();
-});
 
 $(".frontSelector").on("click", function() {
-    $("#projectCarouselFRONT").attr("style", "display: block;")
-    $("#projectCarouselBACK").attr("style", "display: none;")
+    $("#projectCarouselFRONT").attr("style", "display: block; color: white;")
+    $("#projectCarouselBACK").attr("style", "display: none; color: black;")
+    $(".backSelector").attr("style", "color: black;")
+    $(".frontSelector").attr("style", "color: white;")
 });
 
 $(".backSelector").on("click", function() {
-    $("#projectCarouselBACK").attr("style", "display: block;")
-    $("#projectCarouselFRONT").attr("style", "display: none;")
+    $("#projectCarouselBACK").attr("style", "display: block; color: white;")
+    $("#projectCarouselFRONT").attr("style", "display: none; color: black;")
+    $(".backSelector").attr("style", "color: white;")
+    $(".frontSelector").attr("style", "color: black;")
 });
