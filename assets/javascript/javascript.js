58bef62efdeaa7372467160bb598fbe07bb06ea4
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-13 18:38:25 -0400

    made the projects page better, added js, and made background better

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
     new WOW().init();
-  });
+});
+
+$(".frontSelector").on("click", function() {
+    $("#projectCarouselFRONT").attr("style", "display: block;")
+    $("#projectCarouselBACK").attr("style", "display: none;")
+});
+
+$(".backSelector").on("click", function() {
+    $("#projectCarouselBACK").attr("style", "display: block;")
+    $("#projectCarouselFRONT").attr("style", "display: none;")
+});
