43c589a7a4fe6c4ac8c29849fede70f153af721f
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-22 16:40:33 -0400

    added animation to home page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,3 +13,10 @@ $(".backSelector").on("click", function() {
     $(".frontSelector").attr("style", "color: black;")
 });
 
+
+const tl = new TimelineMax();
+
+tl.fromTo($("#headerContainer"), 1, { y: "-100%" }, { y: "0%", ease: Power2.easeInOut })
+.fromTo($("#mainHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0 })
+.fromTo($("#mainSubHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0 })
+.fromTo($("#mainBottom"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0 }, "-=0.4");
