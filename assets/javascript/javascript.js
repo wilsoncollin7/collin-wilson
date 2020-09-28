044403e4831f0ac07399d7d60b38d70f4a0c8ea2
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-28 15:49:59 -0400

    changed whole color scheme to match the main image

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,15 +2,15 @@
 $(".frontSelector").on("click", function() {
     $("#projectCarouselFRONT").attr("style", "display: block; color: white;")
     $("#projectCarouselBACK").attr("style", "display: none; color: black;")
-    $(".backSelector").attr("style", "color: black;")
-    $(".frontSelector").attr("style", "color: white;")
+    $(".backSelector").attr("style", "color: #EEEBE7;")
+    $(".frontSelector").attr("style", "color: #BB4111;")
 });
 
 $(".backSelector").on("click", function() {
     $("#projectCarouselBACK").attr("style", "display: block; color: white;")
     $("#projectCarouselFRONT").attr("style", "display: none; color: black;")
-    $(".backSelector").attr("style", "color: white;")
-    $(".frontSelector").attr("style", "color: black;")
+    $(".backSelector").attr("style", "color: #BB4111;")
+    $(".frontSelector").attr("style", "color: #EEEBE7;")
 });
 
 
@@ -22,3 +22,6 @@ tl.fromTo($("#mainContainer"), 1.2, { opacity: 0, width: "0%" }, { opacity: 1, w
 .fromTo($("#mainSubHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
 .fromTo($(".nav-bar"), 1, {width: "0%", opacity: 0}, {width: "100%", opacity: 1, ease: Power2.easeInOut}, "-=0.4")
 .fromTo($("#mainBottom"), 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0, ease: Power2.easeInOut }, "-=0.4");
+
+
+
