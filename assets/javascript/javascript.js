471590f95991f1fb2ef1e2d160d0f876dd1f5308
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-29 12:06:08 -0400

    cleaned up js file and added openAnim function to start the page

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,33 +1,15 @@
-
-$(".frontSelector").on("click", function() {
-    frontSelected();
-    // $("#projectCarouselFRONT").attr("style", "display: block; color: white;")
-    // $("#projectCarouselBACK").attr("style", "display: none; color: black;")
-    $(".backSelector").attr("style", "color: #EEEBE7;");
-    $(".frontSelector").attr("style", "color: #4B9CD3;");
-});
-
-$(".backSelector").on("click", function() {
-    backSelected();
-    // $("#projectCarouselBACK").attr("style", "display: block; color: white;")
-    // $("#projectCarouselFRONT").attr("style", "display: none; color: black;")
-    $(".backSelector").attr("style", "color: #4B9CD3;");
-    $(".frontSelector").attr("style", "color: #EEEBE7;");
-});
-
-
 const tl = new TimelineMax();
-
-tl.fromTo($("#mainContainer"), 1.2, { opacity: 0, width: "0%" }, { opacity: 1, width: "100%", ease: Power2.easeInOut })
-.fromTo($(".mainHeaderWrapper"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
-.fromTo($("#mainHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
-.fromTo($("#mainSubHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
-.fromTo($(".nav-bar"), 1, {width: "0%", opacity: 0}, {width: "100%", opacity: 1, ease: Power2.easeInOut}, "-=0.4")
-.fromTo($("#mainBottom"), 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0, ease: Power2.easeInOut }, "-=0.4");
-
-
 let isFrontSelected = true;
 let isBackSelected = false;
+
+const openAnim = function() {
+    tl.fromTo($("#mainContainer"), 1.2, { opacity: 0, width: "0%" }, { opacity: 1, width: "100%", ease: Power2.easeInOut })
+    .fromTo($(".mainHeaderWrapper"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
+    .fromTo($("#mainHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
+    .fromTo($("#mainSubHeader"), 0.4, { opacity: 0, x: 30 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
+    .fromTo($(".nav-bar"), 1, {width: "0%", opacity: 0}, {width: "100%", opacity: 1, ease: Power2.easeInOut}, "-=0.4")
+    .fromTo($("#mainBottom"), 1, { opacity: 0, x: 30 }, { opacity: 1, x: 0, ease: Power2.easeInOut }, "-=0.4");
+};
 
 const frontSelected = function() {
     if (isFrontSelected) {
@@ -37,8 +19,8 @@ const frontSelected = function() {
     isBackSelected = false;
 
     tl.fromTo($("#projectCarouselBACK"), 0.4, { opacity: 1, x: 0 }, { opacity: 0, x: 3000, ease: Power2.easeInOut })
-    .fromTo($("#projectCarouselFRONT"), 0.4, { opacity: 0, x: 3000 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
-    $("#projectCarouselFRONT").attr("style", "pointer-events: all;")
+    .fromTo($("#projectCarouselFRONT"), 0.4, { opacity: 0, x: 3000 }, {opacity: 1, x: 0, ease: Power2.easeInOut });
+    $("#projectCarouselFRONT").attr("style", "pointer-events: all;");
 };
 
 const backSelected = function() {
@@ -49,7 +31,20 @@ const backSelected = function() {
     isBackSelected = true;
 
     tl.fromTo($("#projectCarouselFRONT"), 0.4, { opacity: 1, x: 0 }, { opacity: 0, x: 3000, ease: Power2.easeInOut })
-    .fromTo($("#projectCarouselBACK"), 0.4, { opacity: 0, x: 3000 }, {opacity: 1, x: 0, ease: Power2.easeInOut })
-    $("#projectCarouselBACK").attr("style", "pointer-events: all;")
+    .fromTo($("#projectCarouselBACK"), 0.4, { opacity: 0, x: 3000 }, {opacity: 1, x: 0, ease: Power2.easeInOut });
+    $("#projectCarouselBACK").attr("style", "pointer-events: all;");
 };
 
+$(".frontSelector").on("click", function() {
+    frontSelected();
+    $(".backSelector").attr("style", "color: #EEEBE7;");
+    $(".frontSelector").attr("style", "color: #4B9CD3;");
+});
+
+$(".backSelector").on("click", function() {
+    backSelected();
+    $(".backSelector").attr("style", "color: #4B9CD3;");
+    $(".frontSelector").attr("style", "color: #EEEBE7;");
+});
+
+openAnim();
